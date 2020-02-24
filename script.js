46abd663532ec2641dafb926e95ff5c82307da19
Asher <asher_3@hotmail.co.uk>
2020-02-24 11:00:16 +0000

    finishing touches

--- script.js
+++ script.js
@@ -10,34 +10,56 @@ const WINNING_COMBINATIONS = [
 	[ 0, 4, 8 ],
 	[ 2, 4, 6 ]
 ];
-
 const cellElements = document.querySelectorAll('[data-cell]');
-const board = document.getElementById;
+const board = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const restartButton = document.getElementById('restartButton');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
 let circleTurn;
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
 
 function startGame() {
 	circleTurn = false;
 	cellElements.forEach((cell) => {
+		cell.classList.remove(X_CLASS);
+		cell.classList.remove(CIRCLE_CLASS);
+		cell.removeEventListener('click', handleClick);
 		cell.addEventListener('click', handleClick, { once: true });
 	});
-	setBoardHoverClass;
+	setBoardHoverClass();
+	winningMessageElement.classList.remove('show');
 }
-
-cellElements.forEach((cell) => {
-	cell.addEventListener('click', handleClick, { once: true });
-});
 
 function handleClick(e) {
 	const cell = e.target;
 	const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
 	placeMark(cell, currentClass);
+	if (checkWin(currentClass)) {
+		endGame(false);
+	} else if (isDraw()) {
+		endGame(true);
+	} else {
+		swapTurns();
+		setBoardHoverClass();
+	}
+}
 
-	// placeMark
-	//Check For Win
-	//Check For Draw
-	//Switch Turns
-	swapTurns();
-	setBoardHoverClass();
+function endGame(draw) {
+	if (draw) {
+		winningMessageTextElement.innerText = 'Draw!';
+	} else {
+		winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+	}
+	winningMessageElement.classList.add('show');
+}
+
+function isDraw() {
+	return [ ...cellElements ].every((cell) => {
+		return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
+	});
 }
 
 function placeMark(cell, currentClass) {
